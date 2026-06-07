90881f10a1ebc2a5fe0a6274f18b08114ff54bd2
Akpornorsor <akpornorsor@gmail.com>
2026-06-07 00:43:37 +0000

    Build: Clean up tracking states for verification screen

--- lib/presentation/screens/student_verify_details_screen.dart
+++ lib/presentation/screens/student_verify_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:intl/intl.dart';
 import '../../data/services/api_client.dart';
 import 'session_summary_screen.dart';
 
